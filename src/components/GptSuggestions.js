56ce3d177f3590753a0a4cd3f9a7d1e47a047d9e
Vipul Bhardwaj <vipulbhardwaj785@gmail.com>
2024-01-03 12:43:40 +0530

    style: shimmer

--- src/components/GptSuggestions.js
+++ src/components/GptSuggestions.js
@@ -1,13 +1,15 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import MovieList from "./MovieList";
+import GptShimmer from "./GptShimmer";
 
 const GptSuggestions = () => {
   const movieNames = useSelector((store) => store.gpt.gptMovieArray);
   const movieData = useSelector((store) => store.gpt.gptMovieData);
-  if (!movieNames || !movieData) return null;
 
-  return (
+  return !movieNames || !movieData ? (
+    <GptShimmer />
+  ) : (
     <div className="suggestions-main bg-black h-full pb-[39px] shadow-2xl">
       <div className="movie-list md:-mt-[20rem] -mt-[20rem] md:ml-[51px] ml-[21px] relative">
         {movieNames.map((movieName, index) => (
