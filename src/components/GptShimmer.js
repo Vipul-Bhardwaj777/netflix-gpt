25f65e286d79cf0a9f8807f16b18bac6a1e647fb
Vipul Bhardwaj <vipulbhardwaj785@gmail.com>
2024-01-03 12:51:05 +0530

    style: responsive

--- src/components/GptShimmer.js
+++ src/components/GptShimmer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const GptShimmer = () => {
   return (
-    <div className="w-full h-[50%] absolute top-24 flex-center justify-center pt-32 pr-20 text-white text-xl font-bold animate-bounce">
+    <div className="w-full h-[50%] absolute top-24 flex-center justify-center pt-32 xl:pr-20 text-white text-sm md:text-xl font-bold animate-bounce">
       Click on search to see your desired movies!
     </div>
   );
